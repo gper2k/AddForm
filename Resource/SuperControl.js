0b26903302062728a070731d93353ca090a890ba
gper <gper@RYZEN>
2020-06-22 22:25:26 +0900

    공유

--- Resource/SuperControl.js
+++ Resource/SuperControl.js
@@ -12,13 +12,15 @@ var SuperControl;
             this.bind("body");
         }
         Starter.prototype.bind = function (selecter) {
-            var self = this;
+            var sc = this;
             $(selecter).find("super").each(function () {
                 var elm = this;
                 var type = elm.attributes["type"] == undefined ? ControlTypes.text : ControlTypes[elm.attributes["type"].value];
+                if (sc.items.some(function (p) { return p.target == elm; }))
+                    sc.items.splice(sc.items.findIndex(function (p) { return p.target == elm; }), 1);
                 switch (type) {
                     default:
-                        self.items.push(new TextBox(elm));
+                        sc.items.push(new TextBox(elm));
                         break;
                 }
             });
@@ -27,6 +29,19 @@ var SuperControl;
         return Starter;
     }());
     SuperControl.Starter = Starter;
+    function addAttr(node, attName, value) {
+        var wrap = $(node);
+        if (wrap.attr(attName) != value)
+            wrap.attr(attName, value == undefined ? "" : value);
+        return wrap.attr(attName);
+    }
+    function addTag(node, selecter, creater) {
+        var wrap = $(node);
+        if (wrap.children(selecter).length <= 0)
+            wrap.append(creater());
+        return wrap.children(selecter).get(0);
+    }
+    var addTitle = function (ctl) { return addTag(ctl.target, "super-header", function () { return "<super-header>" + ctl.title + "</super-header>"; }); };
     var TextBox = (function () {
         function TextBox(target) {
             this.target = target;
@@ -35,8 +50,17 @@ var SuperControl;
             this.id = this.wrap.attr("id");
             this.title = this.wrap.attr("title");
             this.name = this.wrap.attr("name");
-            console.log(this);
+            this.value = this.wrap.attr("value");
+            this.bind();
         }
+        TextBox.prototype.bind = function () {
+            var _this = this;
+            addTitle(this);
+            var tWrap = addTag(this.target, "super-body", function () { return "<super-body></super-body>"; });
+            var tInp = addTag(tWrap, "input[type=text]", function () { return "<input type=\"text\" value=\"" + (_this.value == undefined ? "" : _this.value) + "\" />"; });
+            addAttr(tInp, "id", this.id);
+            addAttr(tInp, "name", this.name);
+        };
         return TextBox;
     }());
     SuperControl.TextBox = TextBox;
--- TS/SuperControl.ts
+++ TS/SuperControl.ts
@@ -9,7 +9,10 @@ namespace SuperControl {
         title:string;
         target: Element;
         type:ControlTypes;
+        value:string;
+        bind():void;
     }
+
 
     export class Starter {
         items : Array<IControl> = [];
@@ -19,21 +22,33 @@ namespace SuperControl {
             this.bind("body");
         }
         bind(selecter:string):Starter {
-            const self = this;
+            const sc = this;
             $(selecter).find("super").each(function(){
                 const elm = this;
                 const type = elm.attributes["type"] == undefined ? ControlTypes.text : ControlTypes[elm.attributes["type"].value];
+                if(sc.items.some(p => p.target == elm))
+                    sc.items.splice(sc.items.findIndex(p => p.target == elm), 1);                
                 
-                switch(type){
-
+                switch(type) {
                     default:
-                        self.items.push(new TextBox(elm));
+                        sc.items.push(new TextBox(elm));
                         break;
                 }
             });
             return this;
         }
     }
+    function addAttr(node:Element, attName:string, value:any):any {
+        var wrap = $(node);
+        if(wrap.attr(attName) != value) wrap.attr(attName, value == undefined ? "" : value);
+        return wrap.attr(attName);
+    }
+    function addTag(node:Element, selecter:string, creater:() => string):Element {
+        var wrap = $(node);
+        if(wrap.children(selecter).length <= 0) wrap.append(creater());
+        return wrap.children(selecter).get(0);
+    }
+    const addTitle = (ctl:IControl) => addTag(ctl.target, "super-header", () => `<super-header>${ctl.title}</super-header>`);
 
     export class TextBox implements IControl {
         id:string;
@@ -41,6 +56,7 @@ namespace SuperControl {
         title:string;
         wrap:any;
         type:ControlTypes = ControlTypes.text;
+        value:string;
 
         constructor(public target:Element)
         {
@@ -48,7 +64,16 @@ namespace SuperControl {
             this.id = this.wrap.attr("id");
             this.title = this.wrap.attr("title");
             this.name = this.wrap.attr("name");
-            console.log(this);
+            this.value = this.wrap.attr("value");
+            this.bind();
+        }
+
+        bind():void {
+            addTitle(this);
+            const tWrap = addTag(this.target, "super-body", () => `<super-body></super-body>`);
+            const tInp = addTag(tWrap, "input[type=text]", () => `<input type="text" value="${ this.value == undefined ? "" : this.value}" />`);
+            addAttr(tInp, "id", this.id);
+            addAttr(tInp, "name", this.name);
         }
     }
 }
